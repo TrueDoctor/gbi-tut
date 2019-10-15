83cd5b2602ba30f824a19dc13c1e4d82858ba68b
Dennis Kobert <dennis@kobert.dev>
2019-10-15 14:13:50 +0200

    first commit

---
+++ compile3.bat
@@ -0,0 +1,19 @@
+@echo on
+SET Number=03
+echo Recompiling slide %Number%...
+FOR %%f in (*.tex) DO (
+	IF "%%f" == "slide-tut%Number%.tex" (
+        REM PAUSE
+		echo Generating live beamer slides: %%f
+		pdflatex "\input{%%f}"
+		pdflatex "\input{%%f}"
+		MOVE %%~nf.pdf slides\liveslide_%%~nf.pdf
+		::echo Generating printing beamer slides: %%f
+		::pdflatex "\def\compiletype{print} \input{%%f}"
+		::pdflatex "\def\compiletype{print} \input{%%f}"
+		::MOVE %%~nf.pdf slides\printslide_%%~nf.pdf
+    )
+)
+
+cleanup.bat
+pause
